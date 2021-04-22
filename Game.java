880eefb3f5b0822f3fbea237f896667533f8b131
henrik-r3 <79112168+henrik-r3@users.noreply.github.com>
2021-04-22 20:39:00 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,56 +1,56 @@
 import java.util.*;
 import java.awt.*;
-import java.math.*;
 
-public class Game{
+public class Game {
 
     private int bombs;
     private int rows;
     private int columns;
     public static Game instance;
     private char[][] gamePanel;
+    private Random rnd = new Random();
 
-    public Game(String difficulty){
+    public Game(String difficulty) {
 
         instance = this;
-        if(difficulty == "Leicht"){
+        if (difficulty.equals("Leicht")) {
             rows = 9;
             columns = 9;
             bombs = 10;
-        }else if(difficulty == "Mittel"){
+        } else if (difficulty.equals("Mittel")) {
             rows = 16;
             columns = 16;
             bombs = 40;
-        }else if (difficulty == "Schwer"){
+        } else if (difficulty.equals("Schwer")) {
             rows = 16;
             columns = 30;
             bombs = 99;
-        } 
+        }
 
-        gamepanel = new char[rows][columns];
-        
+        gamePanel = new char[rows][columns];
+
     }
 
-    public void calculateBombs(){
-    
-        for(int i = 0; i < bombs; i++){
+    public void calculateBombs() {
 
-            int x = Math.random()*rows;
-            int y = Math.random()*columns;
-            
-            while(gamePanel[x][y] == 'X'){
-    
-                x = Math.random()*rows;
-                y = Math.random()*columns;
+        for (int i = 0; i < bombs; i++) {
+
+            int x = rnd.nextInt(rows);
+            int y = rnd.nextInt(columns);
+
+            while (gamePanel[x][y] == 'X') {
+
+                x = rnd.nextInt(rows);
+                y = rnd.nextInt(columns);
             }
-    
+
             gamePanel[x][y] = 'X';
         }
 
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < columns; j++){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
 
-                if(gamePanel[i][j] != 'X')
+                if (gamePanel[i][j] != 'X')
                     gamePanel[i][j] = '0';
 
             }
@@ -58,56 +58,56 @@ public class Game{
 
     }
 
-    public void calculateNumbers(){
+    public void calculateNumbers() {
 
-
-
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < columns; j++){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
 
                 int bombcounter = 0;
 
-                if(gamePanel[i][j] != 'X'){
+                if (gamePanel[i][j] != 'X') {
 
-                    if(i>0){
-                        if(j>0){
-                            if(gamepanel[i-1][j-1] == 'X')
+                    if (i > 0) {
+                        if (j > 0) {
+                            if (gamePanel[i - 1][j - 1] == 'X')
                                 bombcounter++;
                         }
 
-                        if(gamepanel[i-1][j] == 'X')
+                        if (gamePanel[i - 1][j] == 'X')
                             bombcounter++;
 
-                        if(j< rows-1){
-                            if(gamepanel[i-1][j+1] == 'X')
+                        if (j < rows - 1) {
+                            if (gamePanel[i - 1][j + 1] == 'X')
                                 bombcounter++;
                         }
                     }
 
-                    if(j>0){
-                        if(gamepanel[i][j-1] == 'X')
+                    if (j > 0) {
+                        if (gamePanel[i][j - 1] == 'X')
                             bombcounter++;
                     }
-                    if(j< rows-1){
-                        if(gamepanel[i][j+1] == 'X')
+                    if (j < rows - 1) {
+                        if (gamePanel[i][j + 1] == 'X')
                             bombcounter++;
                     }
 
-                    if(i < columns-1){
-                        if(j>0){
-                            if(gamepanel[i+1][j-1] == 'X')
+                    if (i < columns - 1) {
+                        if (j > 0) {
+                            if (gamePanel[i + 1][j - 1] == 'X')
                                 bombcounter++;
                         }
 
-                        if(gamepanel[i+1][j] == 'X')
+                        if (gamePanel[i + 1][j] == 'X')
                             bombcounter++;
 
-                        if(j< rows-1){
-                            if(gamepanel[i+1][j+1] == 'X')
+                        if (j < rows - 1) {
+                            if (gamePanel[i + 1][j + 1] == 'X')
                                 bombcounter++;
                         }
 
                     }
+
+                    gamePanel[i][j] = (char) bombcounter;
 
                 }
             }
@@ -115,31 +115,33 @@ public class Game{
 
     }
 
-    public void createButtons(){
-
+    public void createButtons() {
+        // create the necessary amaount of buttons in order
     }
 
-    public void getClickEvent(){
-
+    public void getClickEvent() {
+        // get the click event of a button and do actions, left click = activate(use
+        // showPanel), right click = marker
     }
 
-    public void showPanel(){
-
+    public void showPanel() {
+        // show whats beneath a button -> bomb = lost game
     }
 
-    public void showRemainingBombs(){
-
+    public void showRemainingBombs() {
+        // Counter of how many bombs are remaining
     }
 
-    public void showWinScreen(){
-
+    public void showWinScreen() {
+        // Show the WinScreen
     }
 
-    public void showLoseScreen(){
-
+    public void showLoseScreen() {
+        // show the LoseScreen
     }
 
-    public void restartGame(){
-
+    public void restartGame() {
+        // Restart the game(reinitalize the bombs and other fields, make buttons gray
+        // again)
     }
 }
